9305d4c510c03a18fdec6447fa57b3e4cac19f48
saburay32 <saburay32@gmail.com>
2022-02-22 08:56:23 +0300

    работаю с гит

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import News
 
 
-news = [
-    {
-    'title': 'Our first publications',
-    'text': 'Some text',
-    'date': '4.02.2022',
-    'author': 'admin',
-    'ref': 'N+1'
-     },
-    {
-    'title': 'Our second publications',
-    'text': 'Some text',
-    'date': '4.02.2022',
-    #'author': 'Polina',
-    'ref': 'N+1'
-    }
-       ]
+# news = [
+#     {
+#     'title': 'Our first publications',
+#     'text': 'Some text',
+#     'date': '4.02.2022',
+#     'author': 'admin',
+#     'ref': 'N+1'
+#      },
+#     {
+#     'title': 'Our second publications',
+#     'text': 'Some text',
+#     'date': '4.02.2022',
+#     'author': 'Polina',
+#     'ref': 'N+1'
+#     }
+#        ]
 
 def home(request):
     data = {
-        'news': news,
+        'news': News.objects.all(),
         'title': 'Главная страница блога'
     }
     return render(request, 'blog/home.html', data)
