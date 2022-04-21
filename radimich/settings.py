20e5fe31fe9fb38fe257fcb00af5eec975989057
saburay32 <saburay32@gmail.com>
2022-04-21 08:24:24 +0300

    create profile page

--- radimich/settings.py
+++ radimich/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = 'static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
-
+LOGIN_URL = 'user' \
+            ''
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 #from django.contrib.auth.forms import UserCreationForm
 from .forms import UserOurRegistration
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -14,4 +15,8 @@ def register(request):
             return redirect('user')
     else:
         form = UserOurRegistration
-    return render(request, 'users/registration.html', {'form': form, 'title':'Регистрация пользователя'})
+    return render(request, 'users/registration.html', {'form': form, 'title': 'Регистрация пользователя'})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
